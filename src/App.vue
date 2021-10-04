0b56f5ea75214600f939cd7ec17cdd9a761af19a
SkywalkerJi <skywalkerji.cn@gmail.com>
2021-10-04 19:20:01 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Apex Legends </v-list-item-title>
+          <v-list-item-subtitle> @SkywalkerJi </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#fcb69f"
+      dark
+      src="gaming.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Apex Weapon Tools
+      </v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Weapon", icon: "mdi-chemical-weapon", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
+    ],
+    right: null,
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <v-data-table
+      :headers="headers"
+      :items="weapon"
+      :items-per-page="10"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+import weapon from '../assets/weapon'
+export default {
+  name: "Home",
+  data: () => ({
+    headers: [
+      {
+        text: "Weapon",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "Type", value: "class" },
+      { text: "Ammo", value: "ammo" },
+      { text: "Projectile Speed(UPS)", value: "projectileSpeed" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Iron (%)", value: "iron" },
+    ],
+  }),
+  created:function(){
+    this.weapon=weapon
+  }
+
+};
+</script>
