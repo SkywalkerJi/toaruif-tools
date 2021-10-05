601d852fafc1cdc76d433d8a90c46e20b8787aea
SkywalkerJi <skywalkerji.cn@gmail.com>
2021-10-06 00:29:53 +0800

    change title

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,6 @@
   <div class="about">
     <h1>目前仅为测试数据，需要有人帮忙整理。</h1>
     <h1>请联系 <a href="https://twitter.com/Skywalker_Ji">@SkywalkerJi</a></h1>
+    <h1>反馈Q群 <a href="https://jq.qq.com/?_wv=1027&k=1v2Y0f8y">1077682402</a></h1>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,14 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title= 'ToaruIF Tools'
+        return args
+      })
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/toaruif-tools/'
     : '/',
