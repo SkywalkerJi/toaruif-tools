070f289612ffd808fc2fcde73a41cb5b826af9bf
SkywalkerJi <skywalkerji.cn@gmail.com>
2021-10-05 13:36:09 +0800

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>目前仅为测试数据，需要有人帮忙整理。</h1>
+    <h1>请联系 <a href="https://twitter.com/Skywalker_Ji">@SkywalkerJi</a></h1>
   </div>
 </template>
