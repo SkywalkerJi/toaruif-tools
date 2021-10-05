081113c5cbeef8e24508c8af24ad568e9708f2e5
SkywalkerJi <skywalkerji.cn@gmail.com>
2021-10-05 13:31:03 +0800

    v 0.1 demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,86 +3,149 @@
     <v-container>
       <v-row>
         <v-col>
-          <v-select
-            v-model="select"
-            :items="items"
-            :error-messages="errors"
-            label="入手途径"
-            data-vv-name="select"
-            required
-          ></v-select
+          <v-card-title
+            >入手途径:
+            <v-select
+              v-model="cardSearch.obtain"
+              :items="obtain"
+              :error-messages="errors"
+              label="可选择"
+              data-vv-name="select"
+              required
+              @change="searchData"
+            ></v-select> </v-card-title
         ></v-col>
         <v-col>
-          <v-select
-            v-model="select"
-            :items="items"
-            :error-messages="errors"
-            label="阵营"
-            data-vv-name="select"
-            required
-          ></v-select
+          <v-card-title
+            >阵营:
+            <v-select
+              v-model="cardSearch.faction"
+              :items="faction"
+              :error-messages="errors"
+              label="可选择"
+              data-vv-name="select"
+              required
+              @change="searchData"
+            ></v-select></v-card-title
         ></v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-subheader>初始星级</v-subheader>
-          <v-btn-toggle v-model="text" color="deep-purple accent-3" tile group>
-            <v-btn value="☆1"> ☆ </v-btn>
-            <v-btn value="☆2"> ☆☆ </v-btn>
-            <v-btn value="☆3"> ☆☆☆ </v-btn>
-          </v-btn-toggle></v-col
-        >
+          <v-card-title
+            >初始星级:
+            <v-btn-toggle
+              v-model="cardSearch.initialrarity"
+              color="deep-purple accent-3"
+              tile
+              @change="searchData"
+              group
+            >
+              <v-btn value="☆1"> ☆ </v-btn>
+              <v-btn value="☆2"> ☆☆ </v-btn>
+              <v-btn value="☆3"> ☆☆☆ </v-btn>
+            </v-btn-toggle>
+          </v-card-title>
+        </v-col>
         <v-col>
-          <v-subheader>颜色</v-subheader>
-          <v-btn-toggle v-model="text" tile group>
-            <v-btn value="红" color="red"> 红 </v-btn>
-            <v-btn value="蓝" color="blue"> 蓝 </v-btn>
-            <v-btn value="绿" color="green"> 绿 </v-btn>
-            <v-btn value="黄" color="yellow"> 黄 </v-btn>
-            <v-btn value="紫" color="purple"> 紫 </v-btn>
-          </v-btn-toggle>
+          <v-card-title
+            >颜色:
+            <v-btn-toggle
+              v-model="cardSearch.attributes"
+              tile
+              group
+              @change="searchData"
+            >
+              <v-btn value="红" color="red"> 红 </v-btn>
+              <v-btn value="蓝" color="blue"> 蓝 </v-btn>
+              <v-btn value="绿" color="green"> 绿 </v-btn>
+              <v-btn value="黄" color="yellow"> 黄 </v-btn>
+              <v-btn value="紫" color="purple"> 紫 </v-btn>
+            </v-btn-toggle>
+          </v-card-title>
         </v-col>
       </v-row>
       <v-row>
         <v-col>
-          <v-subheader>攻击类型</v-subheader>
-          <v-btn-toggle v-model="text" tile color="deep-purple accent-3" group>
-            <v-btn value="left"> 异能攻击 </v-btn>
+          <v-card-title
+            >攻击类型:
+            <v-btn-toggle
+              v-model="cardSearch.attackMethod"
+              tile
+              @change="searchData"
+              color="deep-purple accent-3"
+              group
+            >
+              <v-btn value="异能攻击"> 异能攻击 </v-btn>
 
-            <v-btn value="center"> 物理攻击 </v-btn>
-          </v-btn-toggle></v-col
+              <v-btn value="物理攻击"> 物理攻击 </v-btn>
+            </v-btn-toggle>
+          </v-card-title></v-col
         >
         <v-col>
-          <v-subheader>攻击方向</v-subheader>
-          <v-btn-toggle
-            v-model="text"
-            tile
-            color="deep-purple accent-3"
-            group
-            multiple
-          >
-            <v-btn value="left"> Left </v-btn>
+          <v-card-title
+            >攻击方向:
+            <v-btn-toggle
+              v-model="cardSearch.attackDirection"
+              tile
+              color="deep-purple accent-3"
+              group
+              @change="searchData"
+              multiple
+            >
+              <v-btn value="1"> ← </v-btn>
 
-            <v-btn value="center"> Center </v-btn>
+              <v-btn value="2"> ↑ </v-btn>
 
-            <v-btn value="right"> Right </v-btn>
-          </v-btn-toggle></v-col
+              <v-btn value="3"> → </v-btn>
+            </v-btn-toggle>
+          </v-card-title>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card-title
+            >卡种:
+            <v-btn-toggle
+              v-model="cardSearch.class"
+              tile
+              @change="searchData"
+              color="deep-purple accent-3"
+              group
+            >
+              <v-btn value="BATTLE"> BATTLE </v-btn>
+
+              <v-btn value="ASSIST"> ASSIST </v-btn>
+            </v-btn-toggle>
+          </v-card-title></v-col
         >
       </v-row>
-      <v-row> </v-row>
     </v-container>
     <v-container>
+      <v-card-title>
+        搜索
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
       <v-data-table
         :headers="headers"
         :items="card"
         :items-per-page="10"
+        :search="search"
         class="elevation-1"
       >
         <template v-slot:item.attributes="{ item }">
-          <v-chip 
-          :color="getColor(item.attributes)" dark>
+          <v-chip :color="getColor(item.attributes)" dark>
             {{ item.attributes }}
           </v-chip>
+        </template>
+        <template v-slot:item.obtain="{ item }">
+          {{ item.limited }}{{ item.obtain }}
         </template>
       </v-data-table>
     </v-container>
@@ -93,32 +156,48 @@
 import card from "../assets/card";
 export default {
   name: "Home",
-  data(){
-    return{
-    items:[],
-    errors:[],
-    select:[],
-    text:[],
-    headers: [
-      {
-        text: "卡名",
-        align: "start",
-        sortable: false,
-        value: "chinese",
+  data() {
+    return {
+      cardSearch: {
+        obtain: "",
+        faction: "",
+        initialrarity: "",
+        attackMethod: "",
+        attackDirection: [],
       },
-      { text: "阵营", value: "faction" },
-      { text: "初始星级", value: "initialrarity" },
-      { text: "颜色", value: "attributes" },
-      { text: "攻击类型", value: "attackMethod" },
-      { text: "HP", value: "hp" },
-      { text: "器用", value: "dexterity" },
-      { text: "异攻", value: "physicalAttack" },
-      { text: "物攻", value: "powerAttack" },
-      { text: "异防", value: "physicalDefense" },
-      { text: "物防", value: "powerDefense" },
-      { text: "入手途径", value: "obtain" },
-    ],
-  }
+      obtain: ["通常", "期间限定", "幻想盛典限定", "活动"],
+      faction: ["科学侧", "魔法侧", "其他阵营", "科学侧 魔法侧"],
+      items: [],
+      errors: [],
+      select: [],
+      text: [],
+      search: "",
+      headers: [
+        {
+          text: "卡名",
+          align: "start",
+          sortable: false,
+          value: "chinese",
+        },
+        { text: "阵营", value: "faction" },
+        { text: "星级", value: "initialrarity" },
+        { text: "颜色", value: "attributes" },
+        { text: "卡种", value: "class" },
+        { text: "类型", value: "attackMethod" },
+        { text: "方向", value: "attackDirection" },
+        // { text: "HP", value: "hp" },
+        // { text: "器用", value: "dexterity" },
+        // { text: "异攻", value: "physicalAttack" },
+        // { text: "物攻", value: "powerAttack" },
+        // { text: "异防", value: "physicalDefense" },
+        // { text: "物防", value: "powerDefense" },
+        { text: "入手", value: "obtain" },
+        { text: "技能", value: "skill1Effect" },
+        { text: "大招", value: "nirvanaEffect" },
+        { text: "潜在1", value: "potentialAbility1Effect" },
+        { text: "潜在2", value: "potentialAbility2Effect" },
+      ],
+    };
   },
   created: function () {
     this.card = card;
@@ -126,10 +205,45 @@ export default {
   },
   methods: {
     getColor(attributes) {
-      console.log(1111);
       if (attributes == "红") return "red";
       else if (attributes == "蓝") return "blue";
       else return "green";
+    },
+    // 查询函数(被查询数组,关键词数组)
+    searchKeysValues(lists, filters) {
+      let resArr = [];
+      lists.filter((item) => {
+        let flag = 1;
+        for (let i in filters) {
+          if (!item[i].includes(filters[i])) {
+            flag = 0;
+          }
+        }
+        if (flag === 1) {
+          resArr.push(item);
+        }
+      });
+      this.card = resArr;
+    },
+    // 筛选非空关键词
+    deleteEmptyKey(obj) {
+      let keys = {};
+      for (let key in obj) {
+        if (obj[key] && obj[key] != "") {
+          keys[key] = obj[key];
+        }
+      }
+      return keys;
+    },
+    searchData() {
+      let keys = this.deleteEmptyKey(this.cardSearch);
+      if (this.cardSearch.attackDirection.length > 0) {
+        keys.attackDirection = "";
+        for (let i = 0; i < this.cardSearch.attackDirection.length; i++) {
+          keys.attackDirection = +this.cardSearch.attackDirection[i];
+        }
+      }
+      this.searchKeysValues(card, keys);
     },
   },
 };
